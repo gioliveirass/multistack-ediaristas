b8c16e8e4c1b1112b69633a39a87675d0474c6b0
Giovana O. Silva <78885451+gioliveirass@users.noreply.github.com>
2021-06-17 15:33:37 -0300

    Criando api para retornar o json com as diaristas cadastradas

---
+++ django/ediaristas_django/api/urls.py
@@ -0,0 +1,8 @@
+# rotas da aplicação api
+
+from django.urls import path
+from .views import DiaristasCidadeList
+
+urlpatterns = [
+    path('diaristas-cidade', DiaristasCidadeList.as_view(), name='diaristas-cidade-list'),
+]
--- django/ediaristas_django/ediaristas_django/urls.py
+++ django/ediaristas_django/ediaristas_django/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('web/', include('web.urls'))
+    path('web/', include('web.urls')),
+    path('api/', include('api.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
